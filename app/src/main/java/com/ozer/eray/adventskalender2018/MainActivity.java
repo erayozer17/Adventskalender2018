e189b5dbf9ad0347fde0b4c8741440661926dcf0
Eray Özer <eray.ozer.91@gmail.com>
2017-10-20 00:33:35 +0300

    20.10

--- app/src/main/java/com/ozer/eray/adventskalender2018/MainActivity.java
+++ app/src/main/java/com/ozer/eray/adventskalender2018/MainActivity.java
@@ -1,12 +1,8 @@
 package com.ozer.eray.adventskalender2018;
 
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
@@ -18,15 +14,21 @@ import java.io.InputStream;
 import java.util.Calendar;
 import java.util.Date;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,
-            imageView9,imageView10,imageView11,imageView12,imageView13,imageView14,imageView15,imageView16,
-            imageView17,imageView18,imageView19,imageView20,imageView21;
+    private ImageView imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,
+            imageView8,imageView9,imageView10,imageView11,imageView12,imageView13,imageView14,
+            imageView15,imageView16,imageView17,imageView18,imageView19,imageView20,imageView21,
+            imageView22,imageView23,imageView24;
 
     private String mesaj = null;
     private Integer gun;
     private Calendar calendar = Calendar.getInstance();
+    private static final int HANGI_AY = 9; // TODO: 19.10.2017 tarihleri ayarlamayı unutma ay 11 olacak (sıfırdan başlıyor)
+    private static final int HANGI_YIL = 2017;
+    private static final int ADVENT_HANGİ_GUN_BASLIYOR = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +36,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         init();
         calendar.setTime(new Date());
+        final int hangiGun = calendar.get(Calendar.DAY_OF_MONTH);
         final int ay = calendar.get(Calendar.MONTH);
         final int yil = calendar.get(Calendar.YEAR);
+        if (!isDateValidToShow(hangiGun,ay,yil)){ // TODO: 19.10.2017 bu kontrolü kurmayı unutma
+            zamaniGelmeyenAlert();
+        }
         ImageView[] tuslar = {imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,
                 imageView7,imageView8,imageView9,imageView10,imageView11,imageView12,imageView13,
                 imageView14,imageView15,imageView16,imageView17,imageView18,imageView19,
-                imageView20,imageView21};
-
+                imageView20,imageView21,imageView22,imageView23,imageView24};
 
         for (ImageView aTuslar : tuslar) {
             aTuslar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     dinle(view);
-                    if ((calendar.get(Calendar.DAY_OF_MONTH) >= gun) && ay == 9 && yil == 2017){
-                        kuralAlertDialogGoster(MainActivity.this);
+                    if (isDateValidToActivateButton(hangiGun,ay,yil)){
+                        kuralAlertDialogGoster();
+                        Toast.makeText(getApplicationContext(),mesaj,Toast.LENGTH_SHORT).show();
                     } else {
                         Toast.makeText(getApplicationContext(),"Daha zamanı değil",Toast.LENGTH_SHORT).show();
                     }
@@ -79,92 +85,107 @@ public class MainActivity extends AppCompatActivity {
         imageView19 = (ImageView) findViewById(R.id.resim19);
         imageView20 = (ImageView) findViewById(R.id.resim20);
         imageView21 = (ImageView) findViewById(R.id.resim21);
+        imageView22 = (ImageView) findViewById(R.id.resim22);
+        imageView23 = (ImageView) findViewById(R.id.resim23);
+        imageView24 = (ImageView) findViewById(R.id.resim24);
     }
 
     public void dinle(View view) {
         switch (view.getId()){
             case R.id.resim1:
-                gun = 3;
+                gun = 1;
                 mesajSetle();
                 break;
             case R.id.resim2:
-                gun = 4;
+                gun = 2;
                 mesajSetle();
                 break;
             case R.id.resim3:
-                gun = 5;
+                gun = 3;
                 mesajSetle();
                 break;
             case R.id.resim4:
-                gun = 6;
+                gun = 4;
                 mesajSetle();
                 break;
             case R.id.resim5:
-                gun = 7;
+                gun = 5;
                 mesajSetle();
                 break;
             case R.id.resim6:
-                gun = 8;
+                gun = 6;
                 mesajSetle();
                 break;
             case R.id.resim7:
-                gun = 9;
+                gun = 7;
                 mesajSetle();
                 break;
             case R.id.resim8:
-                gun = 10;
+                gun = 8;
                 mesajSetle();
                 break;
             case R.id.resim9:
-                gun = 11;
+                gun = 9;
                 mesajSetle();
                 break;
             case R.id.resim10:
-                gun = 12;
+                gun = 10;
                 mesajSetle();
                 break;
             case R.id.resim11:
-                gun = 13;
+                gun = 11;
                 mesajSetle();
                 break;
             case R.id.resim12:
-                gun = 14;
+                gun = 12;
                 mesajSetle();
                 break;
             case R.id.resim13:
-                gun = 15;
+                gun = 13;
                 mesajSetle();
                 break;
             case R.id.resim14:
-                gun = 16;
+                gun = 14;
                 mesajSetle();
                 break;
             case R.id.resim15:
-                gun = 17;
+                gun = 15;
                 mesajSetle();
                 break;
             case R.id.resim16:
-                gun = 18;
+                gun = 16;
                 mesajSetle();
                 break;
             case R.id.resim17:
-                gun = 19;
+                gun = 17;
                 mesajSetle();
                 break;
             case R.id.resim18:
-                gun = 20;
+                gun = 18;
                 mesajSetle();
                 break;
             case R.id.resim19:
-                gun = 21;
+                gun = 19;
                 mesajSetle();
                 break;
             case R.id.resim20:
-                gun = 22;
+                gun = 20;
                 mesajSetle();
                 break;
             case R.id.resim21:
+                gun = 21;
+                mesajSetle();
+                break;
+            case R.id.resim22:
+                gun = 22;
+                mesajSetle();
+                break;
+            case R.id.resim23:
                 gun = 23;
+                mesajSetle();
+                break;
+            case R.id.resim24:
+                gun = 24;
                 mesajSetle();
                 break;
             default:
@@ -203,24 +224,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void kuralAlertDialogGoster(Context context){
-        AlertDialog.Builder builder;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            builder = new AlertDialog.Builder(context, android.R.style.Theme_Material_Dialog_Alert);
-        } else {
-            builder = new AlertDialog.Builder(context);
-        }
-        builder.setTitle("Gunun Kuralı")
-                .setMessage(gununKuraliniGoster())
-                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int which) {
+    public void kuralAlertDialogGoster(){
+        SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+                .setTitleText(gun+ " December")
+                .setContentText("Today's Rule: "+gununKuraliniGoster())
+                .setCustomImage(R.drawable.crystalball)
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
                         Intent i = new Intent(MainActivity.this,FaceTrackerActivity.class);
                         i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                         startActivity(i);
                         finish();
                     }
-                })
-                .show();
+                });
+        pDialog.show();
+    }
+
+    public void zamaniGelmeyenAlert(){
+        SweetAlertDialog pDialog = new SweetAlertDialog(this, SweetAlertDialog.CUSTOM_IMAGE_TYPE)
+                .setTitleText("Hallo")
+                .setContentText("Not Ready Yet :)")
+                .setCustomImage(R.drawable.crystalball)
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sweetAlertDialog) {
+                        finish();
+                    }
+                });
+        pDialog.show();
     }
 
     public String gununKuraliniGoster(){
@@ -245,5 +277,17 @@ public class MainActivity extends AppCompatActivity {
             gununMesajı = "İki gözün kapalı gülme";
         }
         return gununMesajı;
+    }
+
+    private boolean isDateValidToShow(int hangiGun, int ay, int yil){
+        return ay == HANGI_AY
+                && yil == HANGI_YIL
+                && hangiGun >= ADVENT_HANGİ_GUN_BASLIYOR;
+    }
+
+    private boolean isDateValidToActivateButton(int hangiGun, int ay, int yil){
+        return hangiGun >= gun
+                && ay == HANGI_AY
+                && yil == HANGI_YIL;
     }
 }
--- app/src/main/java/com/ozer/eray/adventskalender2018/kameraSonrasiActivity.java
+++ app/src/main/java/com/ozer/eray/adventskalender2018/kameraSonrasiActivity.java
@@ -1,13 +1,38 @@
 package com.ozer.eray.adventskalender2018;
 
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class kameraSonrasiActivity extends AppCompatActivity {
 
+    private static final int VIBRATION_DURATION = 700;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kamera_sonrasi);
+
+        ringRing();
+        vibrate();
+    }
+
+    private void ringRing(){
+        try {
+            Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+            Ringtone r = RingtoneManager.getRingtone(getApplicationContext(), notification);
+            r.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void vibrate(){
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(VIBRATION_DURATION);
     }
 }
